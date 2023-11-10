d0e7f666284565a1dde4a5f57568c3d577a520a9
David Syengo <d.syengo@institute.global>
2023-11-10 17:04:45 +0300

    wip: removed svelte

--- frontend/src/layouts/MainLayout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import NavBar from "./NavBar.svelte";
-
-</script>
-<NavBar />
-<main>
-    <slot></slot>
-</main>
--- frontend/src/pages/IndexPage.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-//
-</script>
-
-<p>This is the index page.</p>
