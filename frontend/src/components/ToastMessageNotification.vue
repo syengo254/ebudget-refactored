673657f68c85708927052a8e251d16e3862a8447
David Syengo <david.syengo019@gmail.com>
2025-07-27 16:08:23 +0300

    wip: added notifications component blank

---
+++ frontend/src/components/ToastMessageNotification.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+type NotificationType = 'success' | 'info' | 'error'
+
+defineProps({
+  type: {
+    type: String as PropType<NotificationType>,
+    required: false,
+    default: 'info',
+  },
+})
+</script>
+
+<template>
+  <div :class="type">
+    <slot name="message">Something happened. Check console logs.</slot>
+  </div>
+</template>
+
+<style scoped>
+.success {
+}
+.info {
+}
+.error {
+}
+</style>
