7ed38f0c8ac7c7c163f2ab0e9269cd3aace278fd
David Syengo <david.syengo019@gmail.com>
2025-08-03 13:35:49 +0300

    fix: updated stuff for cpanel hosting .envs

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
-use App\Actions\SendOrderEmails;
-use App\Enums\OrderStatus;
-use App\Http\Resources\UserResource;
-use App\Mail\OrderCreated;
-use App\Models\Order;
-use App\Models\OrderItem;
-use App\Models\Product;
-use App\Models\Store;
-use App\Services\OrderService;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,17 +20,6 @@ use Illuminate\Support\Facades\Mail;
 Route::get('/', function () {
     return view('index');
 });
-
-// SPA home for redirects
-Route::get("/home", function () {
-    return response()->json([
-        "success" => true,
-        "user" => UserResource::make(Auth::user()),
-        "message" => "You are already logged in.",
-    ], headers: [
-        "Access-Control-Allow-Origin" => "*",
-    ]);
-})->name("home");
 
 
 // email verification URLs
@@ -69,26 +48,13 @@ Route::post('/email/verification-notification', function (Request $request) {
 })->middleware(['auth:sanctum', 'throttle:6,1'])->name('verification.send');
 
 Route::get('/preview', function () {
-    $validated = [
-            'order' => [
-                [
-                    'product_id' => 51,
-                    'count' => 1,
-                ],
-                [
-                    'product_id' => 52,
-                    'count' => 2,
-                ],
-                [
-                    'product_id' => 53,
-                    'count' => 1,
-                ],
-            ],
-            'cart_id' => fake()->uuid(),
-        ];
-    $orderDTO = (new OrderService)->create($validated);
-    if($orderDTO->success){
-        (new SendOrderEmails())->handle($orderDTO->order);
-    }
-    return $orderDTO->toArray();
+    //
+});
+
+// useful for cpanel hosting as SSH terminal access is not available
+Route::get('/setup', function () {
+    Artisan::call('storage:link');
+    Artisan::call('route:cache');
+
+    return 'storage linked and routes cached!';
 });
